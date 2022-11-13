0f05c882d9120858c10f328f1a85e1a9780ff960
Tikidoodlep1 <alex.x.eye@gmail.com>
2022-11-12 20:29:18 -0600

    Added debug mode, hitbox fix, movement fix
    the grave key now toggles all hitboxes and hurtboxes to be drawn as a wireframe, slowing the player down is less jerky, and hitboxes are no longer relative but actual location

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 class hitbox {
 	//Hitboxes
-	constructor(x, y, xSize, ySize, canPass, isCharacterHitbox, character, isStaticHitbox) {
+	constructor(x, y, xSize, ySize, canPass, isCharacterHitbox, character, isStaticHitbox, onCollide) {
+		this.startX = x;
+		this.startY = y;
 		this.x = x;
 		this.y = y;
 		this.width = xSize;
@@ -13,6 +15,8 @@ class hitbox {
 		this.character = character;
 		//Does this hitbox move
 		this.isStaticHitbox = isStaticHitbox;
+		//Any function passed to this variable NEEDS to use the same two parameters (Name doesn't matter)
+		this.onCollide = onCollide;
 		hitboxes[this.index] = this;
 	}
 
@@ -22,9 +26,16 @@ class hitbox {
 	}
 
 	checkCollision() {
+		if(debugMode) {
+			push();
+			fill(255, 0, 255, 0);
+			rect(this.x, this.y, this.x + this.width, this.y + this.height);
+			pop();
+		}
+		
 		//If there is a collision between ANY two hitboxes that exist
 		for(let i = 0; i < hitboxes.length; i++) {
-			//if either hitbox doesn't exist, dont check collision then? \_'-'_/
+			//if either hitbox doesn't exist, dont check collision then? -\_'-'_/-
 			if(typeof this === null || typeof this === undefined || typeof hitboxes[i] === null || typeof hitboxes[i] === undefined) {
 				continue;
 			}
@@ -45,27 +56,19 @@ class hitbox {
 			*/
 			if(i != this.index) {
 				if(hitboxes[i].x+hitboxes[i].width > this.x && hitboxes[i].x < this.x+this.width && hitboxes[i].y+hitboxes[i].height > this.y && hitboxes[i].y < this.y)  {
-					this.onCollision(hitboxes[i], TOP_SIDE);
+					//this.onCollision(hitboxes[i], TOP_SIDE);
+					this.onCollide(hitboxes[i], TOP_SIDE);
 				}else if(hitboxes[i].x+hitboxes[i].width > this.x && hitboxes[i].x < this.x+this.width && hitboxes[i].y+hitboxes[i].height > this.y+this.height-1 && hitboxes[i].y < this.y+this.height) {
-					this.onCollision(hitboxes[i], BOTTOM_SIDE);
+					//this.onCollision(hitboxes[i], BOTTOM_SIDE);
+					this.onCollide(hitboxes[i], BOTTOM_SIDE);
 				}
 				if(hitboxes[i].x+hitboxes[i].width > this.x && hitboxes[i].x < this.x+1 && hitboxes[i].y+hitboxes[i].height > this.y && hitboxes[i].y < this.y+this.height) {
-					this.onCollision(hitboxes[i], LEFT_SIDE);
+					//this.onCollision(hitboxes[i], LEFT_SIDE);
+					this.onCollide(hitboxes[i], LEFT_SIDE);
 				}else if(hitboxes[i].x+hitboxes[i].width-1 > this.x+this.width && hitboxes[i].x < this.x+this.width && hitboxes[i].y+hitboxes[i].height > this.y && hitboxes[i].y < this.y+this.height) {
-					this.onCollision(hitboxes[i], RIGHT_SIDE);
+					//this.onCollision(hitboxes[i], RIGHT_SIDE);
+					this.onCollide(hitboxes[i], RIGHT_SIDE);
 				}
-			}
-		}
-	}
-	
-	onCollision(collisionHitbox, direction) {
-		//We want to check if the collided hitbox is used for a character and if both are unpassable. If so, we need to modify character movement
-		if(collisionHitbox.isCharacterHitbox && !this.canPass && !collisionHitbox.canPass) {
-			//If we're running into the top or bottom, dont cancel the y movement, other wise we're running into the left or right sides and need to cancel the x movement
-			if(direction == TOP_SIDE || direction == BOTTOM_SIDE) {
-				collisionHitbox.character.pos.add(0, 0-collisionHitbox.character.momentum.y);
-			}else {
-				collisionHitbox.character.pos.add(0-collisionHitbox.character.momentum.x, 0);
 			}
 		}
 	}
@@ -77,26 +80,34 @@ class hitbox {
 }
 
 class hurtbox {
-	constructor(x, y, r, attachedHitboxIndex) {
+	constructor(x, y, r, attachedHitboxIndex, onCollide) {
 		this.x = x;
 		this.y = y;
 		this.radius = r;
 		this.attachedHitboxIndex = attachedHitboxIndex;
+		this.onCollide = onCollide;
+	}
+
+	setLoc(x, y) {
+		this.x = x;
+		this.y = y;
 	}
 
 	checkCollision() {
+		if(debugMode) {
+			push();
+			fill(255, 0, 255, 0);
+			ellipse(this.x, this.y, this.radius);
+			pop();
+		}
+
 		for(let i = 0; i < hitboxes.length; i++) {
-			if(i != this.attachedHitboxIndex) {
-				if(dist(hitboxes[i].x + hitboxes[i].width / 2, hitboxes[i].y + hitboxes[i].height / 2, this.x + this.radius, this.y + this.radius) > this.radius) {
-					this.onCollision(hitboxes[i]);
+			if(i != this.attachedHitboxIndex) {				
+				if(dist(hitboxes[i].x + hitboxes[i].width / 2, hitboxes[i].y + hitboxes[i].height / 2, this.x + this.radius, this.y + this.radius) < (hitboxes[i].width / 2) + this.radius) {
+					//this.onCollision(hitboxes[i]);
+					this.onCollide(hitboxes[i]);
 				}
 			}
-		}
-	}
-
-	onCollision(collisionHitbox) {
-		if(collisionHitbox.isCharacterHitbox) {
-			console.log("Hit " + collisionHitbox.character.name + "!");
 		}
 	}
 }
@@ -135,7 +146,7 @@ class character {
 		this.hurtbox = null;
 
 		//character hitbox
-		this.hitbox = new hitbox(this.pos.x - (xSize/2) + backgroundOffsetX, this.pos.y - (ySize/2) + backgroundOffsetY, xSize, ySize, false, true, this, false);
+		this.hitbox = new hitbox(this.pos.x - (xSize/2) + backgroundOffsetX, this.pos.y - (ySize/2) + backgroundOffsetY, xSize, ySize, false, true, this, false, baseCollision);
 		//should the camera follow this character
 		this.shouldCameraFollow = shouldCameraFollow;
 	}
@@ -143,6 +154,8 @@ class character {
 	move(x, y) {
 		//set the absolute location of the hitbox, then move the character pos
 		this.hitbox.setHitboxLoc(this.pos.x + x, this.pos.y + y);
+		this.hitbox.startX = this.pos.x + x;
+		this.hitbox.startY = this.pos.y + y;
 		this.facing = atan2(this.pos.x, this.pos.y) - atan2(x, y);
 		this.pos.add(x, y);
 		if(this.shouldCameraFollow) {
@@ -152,8 +165,7 @@ class character {
 
 	slowDown() {
 		//get the direction the character was heading, then multiply that vector by the momentum. Then move the character and make the momentum -1/8 of it's previous value.
-		let vector = p5.Vector.fromAngle(this.pos.heading()).mult(this.momentum, this.momentum);
-		this.move(vector.x, vector.y);
+		this.move(this.momentum.x, this.momentum.y);
 		this.momentum.add((0-this.momentum.x)/8, (0-this.momentum.y)/8);
 	}
 
@@ -165,19 +177,31 @@ class character {
 
 	performAttack() {
 		if(this.doingMelee) {
+			//rotAngle is based on the frame that we're currently playing. In this case, it's used to determine the "rotation" of the hurtbox in regards to the attacker
 			let rotAngle = (this.attackFrames + 5) / 15;
+			//if the hurtbox is currently null, make one
 			if(this.hurtbox == null) {
-				this.hurtbox = new hurtbox(this.pos.x + (this.xSize/2) + backgroundOffsetX + (70 * cos(this.facing + rotAngle)), this.pos.y + (this.ySize/2) + backgroundOffsetY + (70 * sin(this.facing + rotAngle)), this.xSize / 2, this.hitbox.index);
+				this.hurtbox = new hurtbox(this.pos.x + (this.xSize/2) + backgroundOffsetX + (70 * cos(this.facing + rotAngle)), this.pos.y + (this.ySize/2) + backgroundOffsetY + (70 * sin(this.facing + rotAngle)), this.xSize / 2, this.hitbox.index,
+				//IMPORTANT: Here we are passing a function as a parameter. Check hurtbox#checkCollision() for details on calling this 
+				function(collisionHitbox) {
+					if(collisionHitbox.isCharacterHitbox) {
+						console.log("Hit " + collisionHitbox.character.name + "!");
+					}
+				});
 			}
+			//Draw the hurtbox
 			push();
 			fill(255, 0, 0, 255);
 			ellipse(this.pos.x + (this.xSize/2) + backgroundOffsetX + (70 * cos(this.facing + rotAngle)), this.pos.y + (this.ySize/2) + backgroundOffsetY + (70 * sin(this.facing + rotAngle)), this.xSize / 2);
 			pop();
+			this.hurtbox.setLoc(this.pos.x + (this.xSize/2) + backgroundOffsetX + (70 * cos(this.facing + rotAngle)), this.pos.y + (this.ySize/2) + backgroundOffsetY + (70 * sin(this.facing + rotAngle)) );
+			this.hurtbox.checkCollision();
 			this.attackFrames--;
 			if(this.attackFrames <= 0) {
 				this.doingMelee = false;
+				this.hurtbox = null;
 			}
-			this.hurtbox.checkCollision();
+			
 		}
 	}
 }
@@ -194,6 +218,9 @@ class gameCamera {
 			backgroundOffsetY += this.y - y;
 			this.x = x;
 			this.y = y;
+			for(let i = 0; i < hitboxes.length; i++) {
+				hitboxes[i].setHitboxLoc(hitboxes[i].startX + backgroundOffsetX, hitboxes[i].startY + backgroundOffsetY);
+			}
 		}
 	}
 }
@@ -211,8 +238,10 @@ const sKey = 83;
 const dKey = 68;
 const spaceKey = 32;
 
-//use canvas size / 2 for width and height
+//use canvas size / 2 for x and y
 const mainCamera = new gameCamera(1536/2, 864/2);
+
+let debugMode = false;
 
 //Keep an array of every existing hitbox for checking collision. If a hitbox gets deleted, it MUST be removed from this.
 let hitboxes = [];
@@ -220,9 +249,21 @@ let hitboxes = [];
 let backgroundOffsetX = 0;
 let backgroundOffsetY = 0;
 
+let baseCollision = function(collisionHitbox, direction) {
+		//We want to check if the collided hitbox is used for a character and if both are unpassable. If so, we need to modify character movement
+		if(collisionHitbox.isCharacterHitbox && !this.canPass && !collisionHitbox.canPass) {
+			//If we're running into the top or bottom, dont cancel the y movement, other wise we're running into the left or right sides and need to cancel the x movement
+			if(direction == TOP_SIDE || direction == BOTTOM_SIDE) {
+				collisionHitbox.character.pos.add(0, 0-collisionHitbox.character.momentum.y);
+			}else {
+				collisionHitbox.character.pos.add(0-collisionHitbox.character.momentum.x, 0);
+			}
+		}
+	}
+
 //the player object
 let player = new character("Player", -1, -1, true, true, new p5.Vector(150, 150), 50, 50, 4.0, true);
-let door = new hitbox(400 + backgroundOffsetX, 400 + backgroundOffsetY, 400, 400, false, false, null, true);
+let door = new hitbox(400 + backgroundOffsetX, 400 + backgroundOffsetY, 400, 400, false, false, null, true, baseCollision);
 let enemy1 = new npc(new character("enemy1", -1, -1, false, true, new p5.Vector(300, 180), 120, 120, 0.0, false), false).setEnemyParams(80);
 
 let gameDifficulty = "easy";
@@ -254,16 +295,28 @@ function draw() {
  			hitboxes[i].character.performAttack();
  		}
  	}
+
+ 	if(debugMode) {
+ 		push();
+ 		textSize(42);
+ 		fill(255, 255, 0);
+ 		text("Debug Mode On", width / 2 - 100, 60);
+ 		pop();
+ 	}
 }
 
 function keyPressed() {
 	//console.log(keyCode);
+
+	if(keyCode == 192) { //192 is the grave (`) button
+		debugMode = !debugMode;
+	}
 }
 
 function onMouseClick() {
 	if(player.canMelee && mouseButton === LEFT && !(player.attackFrames > 0)) {
 		player.doingMelee = true;
-		player.attackFrames = 10;
+		player.attackFrames = 7;
 	}
 }
 
